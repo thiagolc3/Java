cbd3d827f786bbc68215f17c2cf337641255626b
Thiago <thiagolc3@gmail.com>
2015-09-11 13:07:45 -0300

    adição do membro valorEstoque

--- src/av1/Estoque.java
+++ src/av1/Estoque.java
@@ -16,4 +16,17 @@ public class Estoque {
     public void novoProduto(int cod, String nome, int preco, int qty){
             produto[cod] = new Produto(nome, preco, qty);
     }
+    
+    public int valorEstoque(){
+
+        int total=0;
+
+        for(int i=0; i<10; i++){
+            if(produto[i]!=null){
+                total+=produto[i].getQuantidade()*produto[i].getQuantidade();
+            }
+        }
+
+        return total;
+    }
 }
